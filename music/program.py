35b066f4ca24856f47b1e3b70bd3c9986ffed893
burkleighhund <85771692+burkleighhund@users.noreply.github.com>
2022-04-03 19:37:21 -0500

    Third Commit

--- music/program.py
+++ music/program.py
@@ -1,4 +1,31 @@
+import string
+from unittest import result
 from assessment import Instructor, Student
+from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy import create_engine
+engine = create_engine('sqlite:///musicallessons.db', echo = True)
+
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+engine = create_engine('sqlite:///musicallessons.db', echo = True)
+meta = MetaData()
+Students=Table(
+    'Students', meta,
+    Column('Id', Integer, primary_key = True),
+    Column('Fname', String),
+    Column('Lname', String),
+    Column('Grade', String),
+    Column('Discipline', String),
+    Column('Instrument', String)
+    )
+    
+Instructors=Table(
+    'Instructors', meta,
+    Column('Id', Integer, primary_key = True),
+    Column('Fname', String),
+    Column('Lname', String),
+    Column('Teaches', String),
+    Column('Discipline', String)
+    )
 
 def main ():
 
@@ -9,6 +36,8 @@ def main ():
         print("Is this a student or instructor? \n Enter done to exit:")
         question = str(input().lower())
         if(question == "student"):
+            print("student id:")
+            id = int(input())
             print("First name:")
             fname = str(input())
             print("Last name:")
@@ -16,13 +45,19 @@ def main ():
             print("What grade is the student in?:")
             grade = str(input())
             print("Are they in Band, Choir, or Orchestra?")
-            dicipline = str(input())
+            discipline = str(input())
             print("What is the student's instrument/voice classification?")
             instrument = str(input())
-            s1 = Student(fname,lname,grade,dicipline,instrument)
+            s1 = Student(fname,lname,grade,discipline,instrument)
             student.append(s1)
+            ins=Students.insert().values(Id=id, Fname=fname, Lname=lname, Grade=grade, Discipline=discipline, Instrument=instrument)
+            conn=engine.connect()
+            result=conn.execute(ins)
         
+
         elif(question == "instructor"):
+            print("instructor id:")
+            id = int(input())
             print("First name:")
             fname = str(input())
             print("Last name:")
@@ -34,6 +69,9 @@ def main ():
             i1 = Instructor(fname,lname,teaches,dicipline)
         #   p1.get_instructor()
             instructor.append(i1)
+            ins=Instructors.insert().values(Id=id, Fname=fname, Lname=lname, Teaches=teaches, Discipline=discipline)
+            conn=engine.connect()
+            result=conn.execute(ins)
         
         else: break
         
